2564ccbc60c2259cec0a08a61448e430cea84e56
xiaowang1984 <14481278@qq.com>
2019-03-14 16:20:14 +0800

    20190314

---
+++ src/main/java/com/neuedu/pojo/EmploymentParams.java
@@ -0,0 +1,12 @@
+package com.neuedu.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class EmploymentParams {
+    private Integer gId;
+    private Date start;
+    private Date end;
+}
