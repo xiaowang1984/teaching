33659086e4f818b339554336c0ea5e2f9bea59d4
xiaowang1984 <14481278@qq.com>
2019-03-27 17:43:20 +0800

    20190327 代码更新为平均值

--- src/main/java/com/neuedu/service/workgrade/IworkGradeService.java
+++ src/main/java/com/neuedu/service/workgrade/IworkGradeService.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface IworkGradeService {
     List<Workgrade> list(int wId);
-    Workgrade getLast(int wId);
+    int getCountByWid(int wId);
     List<Workgrade> workReport(Integer gId,Date start,Date end);
 }
